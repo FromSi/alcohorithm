184158c95431350ee188d5cd4b5238ec801771fb
FromSi <fromsi665@gmail.com>
2021-03-20 20:56:45 +0600

    Create bubble_sort.py

---
+++ bubble_sort.py
@@ -0,0 +1,11 @@
+def make(numbers):
+    N = len(numbers)
+    for i in range(N + 1):
+        for j in range(N - i - 1):
+            if numbers[j] > numbers[j + 1]:
+                numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+    return numbers
+if __name__ == '__main__':
+    numbers = [123, 12, 34, 111, 5]
+    print(make(numbers))
+
