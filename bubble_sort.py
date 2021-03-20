a7eee265409a73a443f0b2227db0c0ba3b88fa50
FromSi <fromsi665@gmail.com>
2021-03-20 21:31:08 +0600

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,11 +1,13 @@
-def make(numbers):
+def handler(numbers):
     N = len(numbers)
+    
     for i in range(N + 1):
         for j in range(N - i - 1):
             if numbers[j] > numbers[j + 1]:
                 numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+                
     return numbers
 
 if __name__ == '__main__':
     numbers = [123, 12, 34, 111, 5]
-    print(make(numbers))
+    print(handler(numbers))
